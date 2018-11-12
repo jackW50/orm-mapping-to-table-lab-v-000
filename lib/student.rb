d83f5f20dd3cb788162abb0e71eab6274316682f
John Werenski <jtwerenski@gmail.com>
2018-11-12 21:15:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ class Student
         VALUES (?, ?)
         SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT")
+    @id = DB[:conn].execute("SELECT last_")
   end
   
 
